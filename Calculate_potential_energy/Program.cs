0ec7da58aa4958c1b5cf25ff596352d2420aba4b
Gabriel Torres <elcomparosh97@gmail.com>
2024-02-20 16:52:55 -0600

    modification in the units in which potential energy is measured

--- Calculate_potential_energy/Program.cs
+++ Calculate_potential_energy/Program.cs
@@ -4,5 +4,5 @@ PotentialEnergy ball = new PotentialEnergy();
 ball.Mass = 15.5;
 ball.Height = 50.3;
 
-Console.WriteLine("The potential energy of the object with mass of: "+ball.Mass+ "kg and height of: "+ball.Height +"m it is: "+ ball.CalculatePotentialEnergy(ball)+" newtons");
+Console.WriteLine("The potential energy of the object with mass of: "+ball.Mass+ " kg and height of: "+ball.Height +" m it is: "+ ball.CalculatePotentialEnergy(ball)+" Joules");
 Console.ReadKey();
