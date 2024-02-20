8d22b17763de3b4ba39568257d6ae0e4d3730a46
Gabriel Torres <elcomparosh97@gmail.com>
2024-02-19 19:08:42 -0600

    Agregar archivos de proyecto.

---
+++ Calculate_potential_energy/PotentialEnergy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_potential_energy
+{
+    internal class PotentialEnergy
+    {
+        private double mass;
+
+        public double Mass
+        {
+            get { return mass; }
+            set { mass = value; }
+        }
+
+        private double height;
+
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public PotentialEnergy()
+        {
+            mass = 0;
+            height = 0;
+        }
+
+        public double CalculatePotentialEnergy()
+        {
+            double gravity = 9.8;
+            return mass * gravity * height;
+        }
+    }
+}
---
+++ Calculate_potential_energy/Program.cs
@@ -0,0 +1,10 @@
+﻿using Calculate_potential_energy;
+
+PotentialEnergy ball = new PotentialEnergy();
+ball.Mass = 15.5;
+ball.Height = 50.3;
+
+double Ep = ball.CalculatePotentialEnergy();
+
+Console.WriteLine("The potential energy is: " + Ep + " joules");
+Console.ReadKey();
